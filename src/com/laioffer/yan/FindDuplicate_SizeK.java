d6b02ef6764a5c22410bcef9ef291de6820be47e
Cory King <corykingsf@gmail.com>
2020-11-26 00:57:37 -0800

    Update sliding window problems

--- src/com/laioffer/yan/FindDuplicate_SizeK.java
+++ src/com/laioffer/yan/FindDuplicate_SizeK.java
@@ -5,10 +5,37 @@ import java.util.*;
 public class FindDuplicate_SizeK {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5,2};
+        int[] arr = new int[]{1,2,3,1,5,2};
         System.out.println(containsNearbyDuplicate(arr, 2));
+        System.out.println(containsNearbyDup(arr, 2));
+    }
+    /*
+    method 1: 记录每个元素最近的出现过的index,如果发现当前元素之前出现过，判断距离时候小于k,如果小于k, return true,
+    否则更新元素最后出现位置的index
+     */
+
+    private static boolean containsNearbyDup(int[] arr, int k) {
+        // 物理意义： key:元素, value: 元素最后出现的Index
+        Map<Integer, Integer> visited = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (visited.containsKey(arr[i])) {
+               if (i - visited.get(arr[i]) <= k ) {
+                   return true;
+               }
+            }
+            visited.put(arr[i], i);
+
+        }
+        for (Map.Entry<Integer, Integer> entry : visited.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+            System.out.println();
+        }
+        return false;
     }
 
+
+    // method 2
     private static boolean containsNearbyDuplicate(int[] arr, int k) {
 
         Set<Integer> window = new HashSet<>();
